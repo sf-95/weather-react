710461b65b59a82495cef4d262b831e6e96db6e6
sf-95 <sfarch95@gmail.com>
2025-02-15 17:52:14 +0000

    Added npm spinner

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Loader from "react-loader-spinner";
 
 export default function Weather(props) {
   function handleResponse(response) {
@@ -11,4 +12,7 @@ export default function Weather(props) {
   let apiKey = "0a521eaf234a3a56f45252fac3c737ad";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
+  return (
+    <Loader type="Puff" color="blue" height={100} width={100} timeout={3000} />
+  );
 }
